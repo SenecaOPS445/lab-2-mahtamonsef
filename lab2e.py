1283fa0cf440febda3283cf2c468ec7f98b6f61c
Mahta Monsef <mmonsef@myseneca.ca>
2024-05-24 22:20:35 -0400

    Lab2e

---
+++ lab2e.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+'''
+Author:  mahta monsef
+Author ID:  mmonsef
+Date Created:  2024/05/24
+
+'''
+
+count = 10
+while count != 0: 
+    print(count)
+    count = count   - 1
+
+print('blast off!')
