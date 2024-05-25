ed3272cf0b49130c2fda6167b7d59a772f17906b
Mahta Monsef <mmonsef@myseneca.ca>
2024-05-24 22:23:28 -0400

    Lab2f

---
+++ lab2f.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+'''
+Author:  mahta monsef
+Author ID:  mmonsef
+Date Created:  2024/05/24
+
+'''
+
+import sys
+
+count = int(sys.argv[1])
+
+while count != 0:
+    print(count)
+    count = count   - 1
+
+print('blast off!')
